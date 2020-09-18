02db54f7983c447f4f9938bfb60ef3ec4a735b22
Charlene CAESAR Lab <q2330416@gmail.com>
2020-09-18 15:03:25 +0800

    Add negative correlation

--- venn3YT_TCGA_TOP2A_Class1_C08_20200918.R
+++ venn3YT_TCGA_TOP2A_Class1_C08_20200918.R
@@ -18,7 +18,7 @@ SARC <- read.csv(paste0(FilesName,"/SARC (TCGA, PanCancer Atlas)_TOP2A.csv"),hea
 # SARC <- c(as.matrix(SARC))
 
 library("data.table")
-SARC_SC08 <- SARC[SARC$Spearman.s.Correlation >= SCvalueP,]
+SARC_SC08 <- SARC[SARC$Spearman.s.Correlation >= SCvalueP|SARC$Spearman.s.Correlation <= SCvalueN,]
 SARC_SC08P005 <- SARC_SC08[SARC_SC08$p.Value <= Pvalue,]
 
 SARC_SC08P005_GeneList <- c(as.matrix(SARC_SC08P005[,1]))
@@ -26,28 +26,28 @@ SARC_SC08P005_GeneList <- c(as.matrix(SARC_SC08P005[,1]))
 
 #B_LGG
 LGG <- read.csv(paste0(FilesName,"/LGG (TCGA, PanCancer Atlas)_TOP2A.csv"),header = T)
-LGG_SC08 <- LGG[LGG$Spearman.s.Correlation >= SCvalueP,]
+LGG_SC08 <- LGG[LGG$Spearman.s.Correlation >= SCvalueP|LGG$Spearman.s.Correlation <= SCvalueN,]
 LGG_SC08P005 <- LGG_SC08[LGG_SC08$p.Value <= Pvalue,]
 
 LGG_SC08P005_GeneList <- c(as.matrix(LGG_SC08P005[,1]))
 
 #C_KIRP
 KIRP <- read.csv(paste0(FilesName,"/KIRP (TCGA, PanCancer Atlas)_TOP2A.csv"),header = T)
-KIRP_SC08 <- KIRP[KIRP$Spearman.s.Correlation >= SCvalueP,]
+KIRP_SC08 <- KIRP[KIRP$Spearman.s.Correlation >= SCvalueP|KIRP$Spearman.s.Correlation <= SCvalueN,]
 KIRP_SC08P005 <- KIRP_SC08[KIRP_SC08$p.Value <= Pvalue,]
 
 KIRP_SC08P005_GeneList <- c(as.matrix(KIRP_SC08P005[,1]))
 
 #D_KIRC
 KIRC <- read.csv(paste0(FilesName,"/KIRC (TCGA, PanCancer Atlas)_TOP2A.csv"),header = T)
-KIRC_SC08 <- KIRC[KIRC$Spearman.s.Correlation >= SCvalueP,]
+KIRC_SC08 <- KIRC[KIRC$Spearman.s.Correlation >= SCvalueP|KIRC$Spearman.s.Correlation <= SCvalueN,]
 KIRC_SC08P005 <- KIRC_SC08[KIRC_SC08$p.Value <= Pvalue,]
 
 KIRC_SC08P005_GeneList <- c(as.matrix(KIRC_SC08P005[,1]))
 
 #E_LIHC
 LIHC <- read.csv(paste0(FilesName,"/LIHC (TCGA, PanCancer Atlas)_TOP2A.csv"),header = T)
-LIHC_SC08 <- LIHC[LIHC$Spearman.s.Correlation >= SCvalueP,]
+LIHC_SC08 <- LIHC[LIHC$Spearman.s.Correlation >= SCvalueP|LIHC$Spearman.s.Correlation <= SCvalueN,]
 LIHC_SC08P005 <- LIHC_SC08[LIHC_SC08$p.Value <= Pvalue,]
 
 LIHC_SC08P005_GeneList <- c(as.matrix(LIHC_SC08P005[,1]))
@@ -69,7 +69,7 @@ E <- na.omit(LIHC_SC08P005_GeneList)
 colorsT <- c("#ed652f", "#c3db0f", "#db750f", "#eb4979", "#cc45ac")
 venn.diagram(x = list(A, B, C, D, E) ,
              category.names = c("SARC", "LGG","KIRP","KIRC","LIHC"),
-             filename = 'Venn20200917Class1SC008V1.png',
+             filename = 'Venn20200918Class1SC008V2.png',
              output=TRUE,
              imagetype="png", 
              scaled = FALSE,
@@ -85,7 +85,7 @@ venn.diagram(x = list(A, B, C, D, E) ,
 # Display saved image
 options(repr.plot.height=12, repr.plot.width= 12)
 library("png")
-pp <- readPNG("Venn20200917Class1SC008V1.png")
+pp <- readPNG("Venn20200918Class1SC008V2.png")
 plot.new() 
 rasterImage(pp,0,0,1,1)
 
