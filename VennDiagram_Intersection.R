2ac9d2a652cb407bf62460224ea5015e496e8082
Charlene CAESAR Lab <q2330416@gmail.com>
2020-09-24 21:44:11 +0800

    Set label color, line color and thickness

--- VennDiagram_Intersection.R
+++ VennDiagram_Intersection.R
@@ -16,8 +16,8 @@ Pvalue = 0.05
 # Color and Size Setting
 colorsT <- c("#ed652f", "#a332c2", "#9e1b47", "#eb4979", "#cc45ac")
 LWD = 1; # Set the thickness of the line
-LCol = "Gray";
-LabelCol = "#ede8ea";
+LCol = "Gray"; # Set the line color
+LabelCol = "#ede8ea"; # Set the label color
 
 # Files name
 AName <- c("SARC")
